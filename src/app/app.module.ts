6df2de7c8113d0f53793e4700461d8af30b4a799
rama-navsoft <rama.Patro@navsoft.in>
2020-07-13 05:26:47 -0700

    two component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+;
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    SignupComponent
+   
   ],
   imports: [
     BrowserModule,
